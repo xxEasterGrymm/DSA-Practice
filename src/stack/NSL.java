a85a14c39e02895de2edd671b5c3a5709877c8cf
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-08-15 15:43:48 +0530

    initial commit

--- src/stack/NSL.java
+++ src/stack/NSL.java
@@ -1,8 +1,8 @@
-/*Nearest Greater to Left - program to find the nearest greater number to its left.
+/*Nearest Smaller to Left - program to find the smaller greater number to its left.
 
 Example -
-        given Array -   {1, 3, 2, 4}
-        Output -        {-1, -1, 3, -1}
+        given Array -   {4, 5,  2, 10, 8}
+        Output -        {-1, 4, -1, 2, 2}
 */
 
 package stack;
@@ -11,7 +11,7 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.Vector;
 
-public class NGL {
+public class NSL {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length of the array: ");
@@ -24,10 +24,10 @@ public class NGL {
         Stack<Integer> s = new Stack<>();
 
         for(int i = 0; i < n; i++) {
-            if(s.size() == 0) v.add(-1);    // stack empty, hence no element is bigger to the left of current element
-            else if(s.size() > 0 && s.peek() > arr[i]) v.add(s.peek());
-            else if(s.size() > 0 && s.peek() <= arr[i]) {
-                while (s.size() > 0 && s.peek() <= arr[i]) s.pop();     // deleting elements from the stack until a greater element is found or stack becomes empty
+            if(s.size() == 0) v.add(-1);    // stack empty, hence no element is smaller to the left of current element
+            else if(s.size() > 0 && s.peek() < arr[i]) v.add(s.peek());
+            else if(s.size() > 0 && s.peek() >= arr[i]) {
+                while (s.size() > 0 && s.peek() >= arr[i]) s.pop();     // deleting elements from the stack until a smaller element is found or stack becomes empty
                 if(s.size() == 0) v.add(-1);
                 else v.add(s.peek());
             }
---
+++ src/stack/NSR.java
@@ -0,0 +1,40 @@
+/*Nearest Smaller to Right - program to find the smaller greater number to its right.
+
+Example -
+        given Array -   {4, 5,  2, 10, 8}
+        Output -        {2, 2, -1, 8, -1}
+*/
+
+package stack;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Vector;
+
+public class NSR {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        Vector<Integer> v = new Vector<>();
+        Stack<Integer> s = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(s.size() == 0) v.add(-1);    // stack empty, hence no element is smaller to the right of current element
+            else if(s.size() > 0 && s.peek() < arr[i]) v.add(s.peek());
+            else if(s.size() > 0 && s.peek() >= arr[i]) {
+                while (s.size() > 0 && s.peek() >= arr[i]) s.pop();     // deleting elements from the stack until a smaller element is found or stack becomes empty
+                if(s.size() == 0) v.add(-1);
+                else v.add(s.peek());
+            }
+            s.push(arr[i]);
+        }
+        Collections.reverse(v);
+        System.out.println(v);
+    }
+}
