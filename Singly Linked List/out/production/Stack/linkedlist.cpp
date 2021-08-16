77a948154f42e2d41e8536c474c389c02aafa996
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-08-16 22:57:08 +0530

    initial commit

---
+++ Singly Linked List/out/production/Stack/linkedlist.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+};
+
+// This function prints the contents of the linked list
+void printList(Node* n) {
+    while(n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main() {
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    // allocate 3 nodes in the heap
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    printList(head);
+
+    return 0;
+}
